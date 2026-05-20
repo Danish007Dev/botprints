b290fb6b57cfd981f13e4eb409c32444cc201993
Danish007Dev <mdanish0852@gmail.com>
2026-05-20 06:20:31 +0530

    FIX- links to open username and their posts

--- src/client/app.js
+++ src/client/app.js
@@ -738,8 +738,8 @@ import { SIGNALS } from '../shared/signals.js';
     var scoreMeta = isInsufficient ? 'Collecting data' : (elevationCount + ' of 6 signals elevated');
     var displayName = missingUsername 
       ? 'Unknown user' 
-      : 'u/<a href="#" class="user-link" data-url="https://reddit.com/user/' + uname + '" title="View Profile">' + uname + '</a>' +
-        '<a href="#" class="search-link" data-url="https://reddit.com/r/' + currentSubreddit + '/search?q=author:' + uname + '&sort=new&restrict_sr=1" title="Search recent posts in ' + currentSubreddit + '">' +
+      : 'u/<a href="https://reddit.com/user/' + uname + '" target="_blank" class="user-link" title="View Profile">' + uname + '</a>' +
+        '<a href="https://reddit.com/r/' + currentSubreddit + '/search?q=author:' + uname + '&sort=new&restrict_sr=1" target="_blank" class="search-link" title="Search recent posts in ' + currentSubreddit + '">' +
         '<svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: -2px; margin-left: 4px;"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg></a>';
     var usernameClass = missingUsername ? ' username-missing' : '';
 
@@ -1023,9 +1023,15 @@ import { SIGNALS } from '../shared/signals.js';
       uLinks[j].addEventListener('click', function(e) {
         e.preventDefault();
         e.stopPropagation();
-        var url = this.getAttribute('data-url');
-        // Standard Devvit navigate message pattern
-        window.parent.postMessage({ type: 'navigate', data: { url: url } }, '*');
+        var url = this.getAttribute('href');
+        if (window.parent) {
+          window.parent.postMessage({
+            type: 'devvit-internal',
+            scope: 0,
+            navigateToUrl: { url: url },
+            effect: { type: 5, navigateToUrl: { url: url } }
+          }, '*');
+        }
       });
     }
 
